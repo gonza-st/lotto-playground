af24d6638eea1f65f57493464059e674b979da54
currenjin <hyun0524e@naver.com>
2025-02-13 13:58:02 +0900

    Docs: TODO

---
+++ java-playground/src/test/java/org/gonza/javaplayground/lotto/LottoTest.java
@@ -0,0 +1,13 @@
+package org.gonza.javaplayground.lotto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoTest {
+
+    @Test
+    void name() {
+
+    }
+}
