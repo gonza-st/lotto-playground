c48ad5f983e98d61cb04be09f0017a7af4f10dc5
leeMK09 <dlaudrb09@naver.com>
2025-02-13 13:29:02 +0900

    refactor: 번호 범위값에 대한 책임을 Range 로 분리

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/domain/lotto/LottoNumber.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/domain/lotto/LottoNumber.kt
@@ -3,17 +3,25 @@ package org.gonza.kotlinplayground.domain.lotto
 import org.gonza.kotlinplayground.domain.lotto.exception.InvalidLottoNumberException
 
 class LottoNumber(private val number: Int) {
-    companion object {
-        private const val MAX_NUMBER_LENGTH = 45
-    }
 
     init {
-        validate(number)
+        Range(number)
     }
 
-    private fun validate(number: Int) {
-        if (number < 0 || number > MAX_NUMBER_LENGTH) {
-            throw InvalidLottoNumberException()
+    class Range(number: Int) {
+        companion object {
+            private const val MAX_NUMBER_LENGTH = 45
+            private const val MIN_NUMBER_LENGTH = 0
+        }
+
+        init {
+            validate(number)
+        }
+
+        private fun validate(number: Int) {
+            if (number < MIN_NUMBER_LENGTH || number > MAX_NUMBER_LENGTH) {
+                throw InvalidLottoNumberException()
+            }
         }
     }
 }
