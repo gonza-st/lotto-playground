63a6ea1f61827aa024485de588beac4571784648
hongxeob <sjun2918@naver.com>
2025-02-13 13:51:41 +0900

    feature: 4. 로또 기계 구현 및 테스트 (1)

---
+++ java-playground/src/main/java/org/gonza/javaplayground/core/LottoMachine.java
@@ -0,0 +1,20 @@
+package org.gonza.javaplayground.core;
+
+import java.math.BigDecimal;
+
+public class LottoMachine {
+
+    public static final int PURCHASE_PRICE_UNIT = 1000;
+
+    public int generateLottoTicket(Money purchasePrice) {
+        if (purchasePrice.getValue().compareTo(BigDecimal.valueOf(PURCHASE_PRICE_UNIT)) < 0) {
+            throw new IllegalArgumentException("로또는 1000원 이상부터 구매할 수 있습니다.");
+        }
+
+        int value = purchasePrice.getValue()
+                .divide(BigDecimal.valueOf(PURCHASE_PRICE_UNIT))
+                .intValue();
+
+        return value;
+    }
+}
---
+++ java-playground/src/test/java/org/gonza/javaplayground/core/LottoMachineTest.java
@@ -0,0 +1,40 @@
+package org.gonza.javaplayground.core;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoMachineTest {
+    @Test
+    @DisplayName("구입 금액에 맞게 로또 티켓을 생성할 수 있다.")
+    void generateLottoTicketTest() throws Exception {
+        BigDecimal purchasePrice1 = new BigDecimal(14000);
+        BigDecimal purchasePrice2 = BigDecimal.valueOf(1000);
+        LottoMachine lottoMachine = new LottoMachine();
+
+        int purchasedTicket1 = lottoMachine.generateLottoTicket(new Money(purchasePrice1));
+        int purchasedTicket2 = lottoMachine.generateLottoTicket(new Money(purchasePrice2));
+
+        assertThat(purchasedTicket1).isEqualTo(14);
+        assertThat(purchasedTicket2).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 500, 999})
+    @DisplayName("로또 구매 가격이 1000원 미만일 경우 예외 발생")
+    void generateLottoTicketFailTest_under1000(int invalidValue) throws Exception {
+        BigDecimal purchasePrice = new BigDecimal(invalidValue);
+        LottoMachine lottoMachine = new LottoMachine();
+        Money invalidMoney = new Money(purchasePrice);
+
+        assertThatThrownBy(() -> lottoMachine.generateLottoTicket(invalidMoney))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또는 1000원 이상부터 구매할 수 있습니다.");
+    }
+}
