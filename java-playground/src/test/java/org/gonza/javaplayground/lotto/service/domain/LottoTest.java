0923ebf1c3b9bf0e79334d4a4ca480d2e559402f
han-0624 <han@wemeetmobility.com>
2025-02-15 01:02:33 +0900

    feat: 로또 id와 카운트 메소드 추가

--- java-playground/src/test/java/org/gonza/javaplayground/lotto/service/domain/LottoTest.java
+++ java-playground/src/test/java/org/gonza/javaplayground/lotto/service/domain/LottoTest.java
@@ -5,21 +5,25 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LottoTest {
-
+    private List<List<Integer>> givenNumbers;
     private Lotto sut;
 
     @BeforeEach
     public void setUp() {
-        List<List<Integer>> givenNumbers = List.of(
+        this.givenNumbers = List.of(
                 List.of(1, 2, 3, 4, 5, 6),
                 List.of(11, 12, 13, 14, 15, 16)
         );
 
         this.sut = new Lotto(givenNumbers);
+    }
+
+    @Test
+    public void should_have_id() {
+        assertNotNull(this.sut.getId());
     }
 
     @Test
@@ -40,5 +44,11 @@ public class LottoTest {
 
         List<List<Integer>> expected = List.of(List.of(1, 2), List.of(13, 14));
         assertEquals(expected, result);
+    }
+
+    @Test
+    public void should_return_total_lines() {
+        Integer count = sut.countLottoLines();
+        assertEquals(givenNumbers.size(), count);
     }
 }
