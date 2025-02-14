b1f4c9b49ae219f751a5ef3b9e21c77cbd0fba99
han-0624 <han@wemeetmobility.com>
2025-02-15 07:53:56 +0900

    feat: 금액 계산기 작성

---
+++ java-playground/src/test/java/org/gonza/javaplayground/lotto/service/price/PriceCalculatorTest.java
@@ -0,0 +1,53 @@
+package org.gonza.javaplayground.lotto.service.price;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PriceCalculatorTest {
+    private static final Integer PRICE = 1000;
+    private static final List<Integer> PRICE_LIST = List.of(0, 0, 0, 5000, 50000, 1500000, 2000000000);
+
+    private PriceCalculator sut;
+
+    @BeforeEach
+    public void setUp() {
+        WinningPriceTable table = new WinningPriceTable(PRICE_LIST);
+        this.sut = new PriceCalculator(PRICE, table);
+    }
+
+    @Test
+    public void should_return_amount_when_price_is_provided() {
+        Integer price =14000;
+        Integer amount = sut.getAvailableAmount(price);
+        assertEquals(14, amount);
+    }
+
+    @Test
+    public void should_return_max_amount_when_change_exists() {
+        Integer price = 3500;
+        Integer amount = sut.getAvailableAmount(price);
+        assertEquals(3, amount);
+    }
+
+    @Test
+    public void should_return_winning_price() {
+        Integer matchedPoint = 3;
+        Integer winning = sut.getWinningPrice(matchedPoint);
+        assertEquals(PRICE_LIST.get(3), winning);
+    }
+
+    @Test
+    public void should_throw_if_point_is_null() {
+        assertThrows(IllegalArgumentException.class, () -> sut.getWinningPrice(null));
+    }
+
+    @Test
+    public void should_throw_if_point_is_negative_number() {
+        assertThrows(IllegalArgumentException.class, () -> sut.getWinningPrice(-1));
+    }
+}
