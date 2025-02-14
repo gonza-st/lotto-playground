1a84015228eeff177073a90b37ad5c94faefbe77
hongxeob <sjun2918@naver.com>
2025-02-14 16:07:23 +0900

    feature: 13. 당첨 번호와 몇개가 같은지 비교 로직 구현

--- java-playground/src/main/java/org/gonza/javaplayground/core/LottoNumber.java
+++ java-playground/src/main/java/org/gonza/javaplayground/core/LottoNumber.java
@@ -3,4 +3,9 @@ package org.gonza.javaplayground.core;
 import java.util.List;
 
 public record LottoNumber(List<Integer> lottoNumbers) {
+    public int getMatchCount(LottoNumber winningNumber) {
+        return (int) lottoNumbers.stream()
+                .filter(number -> winningNumber.lottoNumbers().contains(number))
+                .count();
+    }
 }
