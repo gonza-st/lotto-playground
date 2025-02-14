0f4b44abb7da31fc5e009d21ede6475f61efb0b6
han-0624 <han@wemeetmobility.com>
2025-02-15 00:51:25 +0900

    feat: 랜덤 번호 생성

---
+++ java-playground/src/test/java/org/gonza/javaplayground/lotto/service/utils/RandomNumberGeneratorTest.java
@@ -0,0 +1,44 @@
+package org.gonza.javaplayground.lotto.service.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RandomNumberGeneratorTest {
+
+    private RandomNumberGenerator sut;
+
+    @BeforeEach
+    public void setUp() {
+        sut = new RandomNumberGenerator();
+    }
+
+    @Test
+    public void should_generate_number_between_min_max_and_specific_size() {
+        Integer size = 6;
+        Integer min = 1;
+        Integer max = 6;
+
+        List<Integer> numbers = sut.generate(size, min, max);
+
+        Integer minNumber = numbers.stream().min(Integer::compareTo).get();
+        Integer maxNumber = numbers.stream().max(Integer::compareTo).get();
+
+        assertEquals(min, minNumber);
+        assertEquals(max, maxNumber);
+        assertEquals(size, numbers.size());
+    }
+
+    @Test
+    public void should_throw_if_min_is_larger_than_max() {
+        Integer size = 6;
+        Integer min = 6;
+        Integer max = 1;
+
+        assertThrows(IllegalArgumentException.class, () -> sut.generate(size, min, max));
+    }
+}
