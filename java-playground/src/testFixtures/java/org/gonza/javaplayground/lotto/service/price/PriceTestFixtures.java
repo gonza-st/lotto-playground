8b60d2914f4ee71dd86682c68fa2bc3603c971bb
han-0624 <han@wemeetmobility.com>
2025-02-15 08:16:49 +0900

    refac: 반복되는 상수 픽스쳐로 분리

---
+++ java-playground/src/testFixtures/java/org/gonza/javaplayground/lotto/service/price/PriceTestFixtures.java
@@ -0,0 +1,8 @@
+package org.gonza.javaplayground.lotto.service.price;
+
+import java.util.List;
+
+public class PriceTestFixtures {
+    public static final Integer PRICE = 1000;
+    public static final List<Integer> WINNING_PRICE_RANGE = List.of(0, 0, 0, 5000, 50000, 1500000, 2000000000);
+}
