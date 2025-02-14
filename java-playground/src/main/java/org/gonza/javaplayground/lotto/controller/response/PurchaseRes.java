393c75f7d81895647ae673e24b0f6af61625d1db
han-0624 <han@wemeetmobility.com>
2025-02-15 08:56:24 +0900

    refac: 구매 응답 객체 수정 및 로또 객체 getter 추가

--- java-playground/src/main/java/org/gonza/javaplayground/lotto/controller/response/PurchaseRes.java
+++ java-playground/src/main/java/org/gonza/javaplayground/lotto/controller/response/PurchaseRes.java
@@ -1,6 +1,27 @@
 package org.gonza.javaplayground.lotto.controller.response;
 
-public record PurchaseRes(
-        String data
-) {
+import org.gonza.javaplayground.lotto.service.lotto.Lotto;
+
+import java.util.List;
+
+public class PurchaseRes {
+    private final Integer count;
+    private final List<List<Integer>> lottoNumbers;
+
+    private PurchaseRes(Integer count, List<List<Integer>> lottoNumbers) {
+        this.count = count;
+        this.lottoNumbers = lottoNumbers;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public List<List<Integer>> getLottoNumbers() {
+        return lottoNumbers;
+    }
+
+    public static PurchaseRes of(Integer count, Lotto lotto) {
+        return new PurchaseRes(count, lotto.getAllLottoNumbers());
+    }
 }
