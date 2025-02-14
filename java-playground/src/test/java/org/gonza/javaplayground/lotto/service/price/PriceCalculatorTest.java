7a1ab627df9750ea1ea10b017a7bb2febe605d6d
han-0624 <han@wemeetmobility.com>
2025-02-15 08:53:58 +0900

    feat: 가격 벨리데이션 추가

--- java-playground/src/test/java/org/gonza/javaplayground/lotto/service/price/PriceCalculatorTest.java
+++ java-playground/src/test/java/org/gonza/javaplayground/lotto/service/price/PriceCalculatorTest.java
@@ -1,6 +1,7 @@
 package org.gonza.javaplayground.lotto.service.price;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -20,34 +21,49 @@ public class PriceCalculatorTest {
         this.sut = new PriceCalculator(PRICE, table);
     }
 
-    @Test
-    public void should_return_amount_when_price_is_provided() {
-        Integer price =14000;
-        Integer amount = sut.getAvailableAmount(price);
-        assertEquals(14, amount);
+    @Nested
+    class GetAvailableAmountTest {
+        @Test
+        public void should_return_amount_when_price_is_provided() {
+            Integer price = 14000;
+            Integer amount = sut.getAvailableAmount(price);
+            assertEquals(14, amount);
+        }
+
+        @Test
+        public void should_return_max_amount_when_change_exists() {
+            Integer price = 3500;
+            Integer amount = sut.getAvailableAmount(price);
+            assertEquals(3, amount);
+        }
+
+        @Test
+        public void should_throw_an_exception_when_price_is_out_of_range() {
+            Integer negativePrice = -1;
+
+            assertThrows(IllegalArgumentException.class, () -> {
+                sut.getAvailableAmount(negativePrice);
+            });
+        }
     }
 
-    @Test
-    public void should_return_max_amount_when_change_exists() {
-        Integer price = 3500;
-        Integer amount = sut.getAvailableAmount(price);
-        assertEquals(3, amount);
-    }
+    @Nested
+    class WinningPriceTest {
+        @Test
+        public void should_return_winning_price() {
+            Integer matchedPoint = 3;
+            Integer winning = sut.getWinningPrice(matchedPoint);
+            assertEquals(PRICE_LIST.get(3), winning);
+        }
 
-    @Test
-    public void should_return_winning_price() {
-        Integer matchedPoint = 3;
-        Integer winning = sut.getWinningPrice(matchedPoint);
-        assertEquals(PRICE_LIST.get(3), winning);
-    }
+        @Test
+        public void should_throw_if_point_is_null() {
+            assertThrows(IllegalArgumentException.class, () -> sut.getWinningPrice(null));
+        }
 
-    @Test
-    public void should_throw_if_point_is_null() {
-        assertThrows(IllegalArgumentException.class, () -> sut.getWinningPrice(null));
-    }
-
-    @Test
-    public void should_throw_if_point_is_negative_number() {
-        assertThrows(IllegalArgumentException.class, () -> sut.getWinningPrice(-1));
+        @Test
+        public void should_throw_if_point_is_negative_number() {
+            assertThrows(IllegalArgumentException.class, () -> sut.getWinningPrice(-1));
+        }
     }
 }
