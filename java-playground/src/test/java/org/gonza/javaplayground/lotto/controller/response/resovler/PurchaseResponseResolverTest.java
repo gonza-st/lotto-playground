617363ba00576705853eb982d1f726a7d8752b96
han-0624 <han@wemeetmobility.com>
2025-02-15 09:12:06 +0900

    feat: 응답값 포맷팅

---
+++ java-playground/src/test/java/org/gonza/javaplayground/lotto/controller/response/resovler/PurchaseResponseResolverTest.java
@@ -0,0 +1,47 @@
+package org.gonza.javaplayground.lotto.controller.response.resovler;
+
+import org.gonza.javaplayground.lotto.controller.response.PurchaseRes;
+import org.gonza.javaplayground.lotto.controller.response.resolver.PurchaseResponseResolver;
+import org.gonza.javaplayground.lotto.ui.LottoResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PurchaseResponseResolverTest {
+
+    private PurchaseResponseResolver sut;
+
+    @BeforeEach
+    public void setUp() {
+        this.sut = new PurchaseResponseResolver();
+    }
+
+    @Nested
+    class HandlePurchaseTest {
+        @Test
+        public void should_return_format_purchase_response() {
+            PurchaseRes res = PurchaseRes.of(1, List.of(List.of(1,2,3)));
+            LottoResponse lottoResponse = sut.resolve(res);
+
+            String formatted = format(res);
+            assertEquals(formatted, lottoResponse.data());
+        }
+
+        private String format(PurchaseRes response) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(response.getCount()).append("개를 구매했습니다.\n");
+
+            response.getLottoNumbers().forEach((lottoNumber) -> {
+                stringBuilder.append(lottoNumber).append("\n");
+            });
+
+            return stringBuilder.toString().trim();
+        }
+    }
+
+
+}
