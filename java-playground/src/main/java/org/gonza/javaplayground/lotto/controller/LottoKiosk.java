61f5ce6d67dfa11dd07d617951e270a1a9918b66
han-0624 <han@wemeetmobility.com>
2025-02-15 09:12:01 +0900

    feat: 키오스크 객체의 구매 메소드 작성

--- java-playground/src/main/java/org/gonza/javaplayground/lotto/controller/LottoKiosk.java
+++ java-playground/src/main/java/org/gonza/javaplayground/lotto/controller/LottoKiosk.java
@@ -4,12 +4,24 @@ import org.gonza.javaplayground.lotto.controller.request.MatchReq;
 import org.gonza.javaplayground.lotto.controller.request.PurchaseReq;
 import org.gonza.javaplayground.lotto.controller.response.PurchaseRes;
 import org.gonza.javaplayground.framework.mapper.Controller;
+import org.gonza.javaplayground.lotto.service.lotto.Lotto;
+import org.gonza.javaplayground.lotto.service.lotto.LottoFactory;
+import org.gonza.javaplayground.lotto.service.price.PriceCalculator;
 
 public class LottoKiosk implements Controller {
+    private final LottoFactory lottoFactory;
+    private final PriceCalculator priceCalculator;
+
+    public LottoKiosk(LottoFactory lottoFactory, PriceCalculator priceCalculator) {
+        this.lottoFactory = lottoFactory;
+        this.priceCalculator = priceCalculator;
+    }
 
     public PurchaseRes handlePurchase(PurchaseReq req) {
-        PurchaseRes res = new PurchaseRes("You purchased a lotto.");
-        return res;
+        Integer count = priceCalculator.getAvailableAmount(req.payment());
+        Lotto lotto = lottoFactory.createLotto(count);
+
+        return PurchaseRes.of(count, lotto.getAllLottoNumbers());
     }
 
     public void handleMatchNumbers(MatchReq req) {
