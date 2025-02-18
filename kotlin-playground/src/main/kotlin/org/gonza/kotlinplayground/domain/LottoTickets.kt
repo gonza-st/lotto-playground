0d92c0195836ae40f0c5e2797aa8501ee6bf80e5
Heonbyeong <abh0920one@gmail.com>
2025-02-18 19:11:57 +0900

    feat: lotto tickets 개발중

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/domain/LottoTickets.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/domain/LottoTickets.kt
@@ -2,4 +2,10 @@ package org.gonza.kotlinplayground.domain
 
 class LottoTickets(val lottoList: List<Lotto>) {
 
+    private val winnerMap: MutableMap<Int, List<Lotto>> = mutableMapOf()
+
+    fun findWinner(answer: List<LottoNumber>): Map<Int, List<Lotto>> {
+        // TODO
+        return winnerMap
+    }
 }
