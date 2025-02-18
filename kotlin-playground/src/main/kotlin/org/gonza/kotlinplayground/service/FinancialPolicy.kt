3227f291a28a80a0ec9cd7ea6935e97c75d96d31
leeMK09 <dlaudrb09@naver.com>
2025-02-18 20:48:14 +0900

    refactor: 로또 가격에 대한 정책을 수익 정책에서 제거

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/service/FinancialPolicy.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/service/FinancialPolicy.kt
@@ -1,7 +1,8 @@
 package org.gonza.kotlinplayground.service
 
 interface FinancialPolicy {
-    fun getPrice(): Int
-
-    fun calculateReturnOnInvestment(totalInvestment: Double, profitAmount: Double): Double
+    fun calculateReturnOnInvestment(
+        totalInvestment: Double,
+        profitAmount: Double,
+    ): Double
 }
