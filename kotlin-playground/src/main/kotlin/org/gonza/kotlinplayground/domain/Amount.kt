a748e813cce60175cae01ac43cddca7b97168569
Heon <abh0920one@gmail.com>
2025-02-18 23:24:24 +0900

    feat: 로또 구매 가능 매수

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/domain/Amount.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/domain/Amount.kt
@@ -1,6 +1,11 @@
 package org.gonza.kotlinplayground.domain
 
+import org.gonza.kotlinplayground.utils.LottoConstants
+
 class Amount(val total: Int) {
+    val maxPurchase: Int
+        get() = total / LottoConstants.LOTTO_PRICE
+
     init {
         require(total > 0) { "금액은 양수만 가능합니다." }
     }
--- kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/AmountTest.kt
+++ kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/AmountTest.kt
@@ -1,9 +1,11 @@
 package org.gonza.kotlinplayground
 
 import org.gonza.kotlinplayground.domain.Amount
+import org.gonza.kotlinplayground.utils.LottoConstants
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
 
 class AmountTest {
     @Test
@@ -25,5 +27,13 @@ class AmountTest {
         assertThrows<IllegalArgumentException> {
             Amount(total = -1000)
         }
+    }
+
+    @Test
+    fun `구매할 수 있는 로또 매수를 가진다`() {
+        val amount = Amount(total = 14000)
+        val expectedCount = amount.total / LottoConstants.LOTTO_PRICE
+
+        assertEquals(expectedCount, amount.maxPurchase)
     }
 }
