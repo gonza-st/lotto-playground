3b6a39c5b11833e0322f8a9b41edf596cd1ebbff
leeMK09 <dlaudrb09@naver.com>
2025-02-19 14:20:03 +0900

    feat: UI 관련 로직 추가

---
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/presentation/OutputView.kt
@@ -0,0 +1,45 @@
+package org.gonza.kotlinplayground.presentation
+
+import org.gonza.kotlinplayground.domain.payment.LottoStatisticsSheet
+
+class OutputView {
+    fun printRequestPurchasePrice() {
+        println("구입금액을 입력해 주세요.")
+    }
+
+    fun printPurchasePrice(purchasePrice: String) {
+        println(purchasePrice)
+    }
+
+    fun printPurchaseLottoTicketCount(ticketCount: Int) {
+        println("$ticketCount 개를 구매했습니다")
+    }
+
+    fun printGeneratedLottoTicketList(ticketList: List<String>) {
+        ticketList.forEach { println(it) }
+    }
+
+    fun printRequestResultLottoTicket() {
+        println("당첨 번호를 입력해 주세요.")
+    }
+
+    fun printResultLottoTicket(resultLottoTicket: String) {
+        println(resultLottoTicket)
+    }
+
+    fun printNoticeStatisticSheet() {
+        println("당첨 통계")
+        println("---------")
+    }
+
+    fun printStatisticSheet(
+        statisticsSheet: LottoStatisticsSheet,
+        matchedCount: Int,
+    ) {
+        println("${statisticsSheet.matchedCount}개 일치 (${statisticsSheet.amount}원) - $matchedCount")
+    }
+
+    fun printReturnOnInvestment(returnOnInvestment: Double) {
+        println("총 수익률은 $returnOnInvestment 입니다.")
+    }
+}
