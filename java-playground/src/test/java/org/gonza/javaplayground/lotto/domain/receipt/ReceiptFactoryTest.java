fedf084cd3879189c74110f3a95d62ea1de378c7
han-0624 <han@wemeetmobility.com>
2025-02-20 08:54:48 +0900

    feat: 영수증 팩토리 테스트 추가

--- java-playground/src/test/java/org/gonza/javaplayground/lotto/domain/receipt/ReceiptFactoryTest.java
+++ java-playground/src/test/java/org/gonza/javaplayground/lotto/domain/receipt/ReceiptFactoryTest.java
@@ -1,14 +1,15 @@
 package org.gonza.javaplayground.lotto.domain.receipt;
 
+import org.gonza.javaplayground.lotto.domain.coin.Purchase;
 import org.gonza.javaplayground.lotto.domain.lotto.LottoLineResult;
 import org.gonza.javaplayground.lotto.domain.lotto.LottoResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ReceiptFactoryTest {
     private static final List<Integer> PRICE_LIST = PriceTestFixtures.WINNING_PRICE_RANGE;
@@ -21,13 +22,18 @@ public class ReceiptFactoryTest {
         this.sut = new ReceiptFactory(table);
     }
 
-//    @Test
-//    public void should_return_winning_Receipt() {
-//        LottoLineResult lineResult = new LottoLineResult(List.of(1,2,3));
-//        List<LottoLineResult> lineResults = List.of(lineResult);
-//        LottoResult result = new LottoResult(lineResults);
-//
-//        Receipt receipt = sut.printReceipt(result);
-//        assertEquals(PRICE_LIST.get(3), winning);
-//    }
+    @Test
+    public void should_return_winning_Receipt() {
+        Purchase purchase = new Purchase(1000);
+
+        String lottoId = UUID.randomUUID().toString();
+        LottoLineResult lineResult = new LottoLineResult(List.of(1,2,3));
+        List<LottoLineResult> lineResults = List.of(lineResult);
+        LottoResult result = new LottoResult(lottoId, lineResults);
+
+        Receipt receipt = sut.printReceipt(purchase, result);
+
+        Double expectedProfit = PRICE_LIST.get(3) / 1000.0;
+        assertEquals(expectedProfit, receipt.getProfit());
+    }
 }
