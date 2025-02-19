eb59a898c0ca52d07b08bf1db577d06ce4bddd76
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2025-02-19 14:08:36 +0900

    feat: LottoNumber를 Set collection으로 가지는 Lotto 생성 및 검증
    LottoNumber를 Set collection으로 가지는 Lotto 생성 및 검증

---
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/domain/Lotto.kt
@@ -0,0 +1,68 @@
+import org.gonza.kotlinplayground.domain.LottoNumber
+import org.gonza.kotlinplayground.enum.LottoRange
+
+class Lotto private constructor(
+    numberSet: Set<LottoNumber>,
+) {
+    private var lottoNumberSet: Set<LottoNumber> = numberSet
+
+    init {
+        require(lottoNumberSet.size == LottoRange.SIZE.value) {
+            "로또 번호는 ${LottoRange.SIZE.value}개여야 합니다."
+        }
+        lottoNumberSet = lottoNumberSet.sortedBy { it.value }.toMutableSet()
+    }
+
+    companion object {
+        private const val SPLIT_WORD = ","
+        private val REGEX = Regex("^[0-9,\\s]*$")
+
+        fun create(winningLottoNumber: String): Lotto {
+            require(REGEX.matches(winningLottoNumber)) {
+                "로또 번호는 숫자와 쉼표(,)만 입력 가능합니다."
+            }
+            val numbers =
+                winningLottoNumber
+                    .split(SPLIT_WORD)
+                    .map { LottoNumber(it.trim().toInt()) }
+                    .toSet()
+            return Lotto(numbers)
+        }
+
+        fun create(numberList: List<Int>): Lotto {
+            val numbers = numberList.map { LottoNumber(it) }.toSet()
+            return Lotto(numbers)
+        }
+
+        fun create(lottoNumberSet: Set<LottoNumber>): Lotto = Lotto(numberSet = lottoNumberSet)
+
+        fun create(): Lotto {
+            val numbers = mutableSetOf<LottoNumber>()
+            while (numbers.size < LottoRange.SIZE.value) {
+                val number = (LottoRange.START.value..LottoRange.END.value).random()
+                numbers.add(LottoNumber(number))
+            }
+            return Lotto(numbers)
+        }
+    }
+
+    fun print(): String =
+        lottoNumberSet.joinToString(
+            separator = ", ",
+            prefix = "[",
+            postfix = "]",
+        ) { it.print() }
+
+    fun matchCount(other: Lotto): Int = lottoNumberSet.intersect(other.lottoNumberSet).size
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Lotto) return false
+
+        if (lottoNumberSet != other.lottoNumberSet) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int = lottoNumberSet.hashCode()
+}
