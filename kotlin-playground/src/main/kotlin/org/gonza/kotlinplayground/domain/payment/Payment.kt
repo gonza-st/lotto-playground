af4c6b7550f107bf397200e33feb35babbc4d8aa
leeMK09 <dlaudrb09@naver.com>
2025-02-21 13:58:19 +0900

    feat: 결제 정보의 금액에 대한 예외처리 추가

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/domain/payment/Payment.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/domain/payment/Payment.kt
@@ -1,5 +1,7 @@
 package org.gonza.kotlinplayground.domain.payment
 
+import org.gonza.kotlinplayground.domain.payment.exception.InvalidPaidMoneyException
+
 class Payment(
     private val paidMoney: Int,
 ) {
@@ -7,7 +9,17 @@ class Payment(
         private const val PRICE = 1_000
     }
 
+    init {
+        validate(paidMoney)
+    }
+
     fun getPaidTicketCount() = paidMoney / PRICE
 
     fun getPaidMoney() = paidMoney
+
+    private fun validate(paidMoney: Int) {
+        if (paidMoney <= 0) {
+            throw InvalidPaidMoneyException()
+        }
+    }
 }
--- kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/domain/payment/PaymentTest.kt
+++ kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/domain/payment/PaymentTest.kt
@@ -1,5 +1,6 @@
 package org.gonza.kotlinplayground.domain.payment
 
+import org.gonza.kotlinplayground.domain.payment.exception.InvalidPaidMoneyException
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
@@ -12,5 +13,23 @@ class PaymentTest {
 
         val expected = 14
         assertEquals(expected, result)
+    }
+
+    @Test
+    fun `결제한 금액이 음수라면 예외가 발생한다`() {
+        val paidMoney = -1
+
+        assertThrows(InvalidPaidMoneyException::class.java) {
+            Payment(paidMoney)
+        }
+    }
+
+    @Test
+    fun `결제한 금액이 0이라면 예외가 발생한다`() {
+        val paidMoney = 0
+
+        assertThrows(InvalidPaidMoneyException::class.java) {
+            Payment(paidMoney)
+        }
     }
 }
