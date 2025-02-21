bc4d31f327b303b0eb88a19469091a3dd1f17b01
han-0624 <han@wemeetmobility.com>
2025-02-21 15:55:47 +0900

    fix: 두 가지 타입의 결과 클래스

--- java-playground/src/main/java/org/gonza/javaplayground/lotto/domain/lotto/result/LottoResultList.java
+++ java-playground/src/main/java/org/gonza/javaplayground/lotto/domain/lotto/result/LottoResultList.java
@@ -1,18 +1,24 @@
 package org.gonza.javaplayground.lotto.domain.lotto.result;
 
 import java.util.List;
+import java.util.Objects;
 
 public class LottoResultList {
     private final String lottoId;
     private final List<LottoLineResult> lineResults;
 
     public LottoResultList(String lottoId, List<LottoLineResult> lineResults) {
-        validate(lineResults);
+        validate(lottoId, lineResults);
+
         this.lottoId = lottoId;
         this.lineResults = lineResults;
     }
 
-    private void validate(List<LottoLineResult> items) {
+    private void validate(String lottoId, List<LottoLineResult> items) {
+        if (Objects.isNull(lottoId)) {
+            throw new IllegalArgumentException("lottoId cannot be null");
+        }
+
         if (items.isEmpty()) {
             throw new IllegalArgumentException("No items found");
         }
--- java-playground/src/main/java/org/gonza/javaplayground/lotto/domain/lotto/result/LottoResultMap.java
+++ java-playground/src/main/java/org/gonza/javaplayground/lotto/domain/lotto/result/LottoResultMap.java
@@ -2,15 +2,28 @@ package org.gonza.javaplayground.lotto.domain.lotto.result;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class LottoResultMap {
     private final String lottoId;
     private final Map<Integer, List<LottoLineResult>> lineResultMap;
 
-    private LottoResultMap(String lottoId, Map<Integer, List<LottoLineResult>> lineResultMap) {
+    public LottoResultMap(String lottoId, Map<Integer, List<LottoLineResult>> lineResultMap) {
+        validation(lottoId, lineResultMap);
+
         this.lottoId = lottoId;
         this.lineResultMap = lineResultMap;
+    }
+
+    private void validation(String lottoId, Map<Integer, List<LottoLineResult>> lineResultMap) {
+        if (Objects.isNull(lottoId)) {
+            throw new IllegalArgumentException("lottoId cannot be null");
+        }
+
+        if (lineResultMap.isEmpty()) {
+            throw new IllegalArgumentException("The map is empty");
+        }
     }
 
     public String getLottoId() {
