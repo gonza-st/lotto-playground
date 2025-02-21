f323eea680e4e1e39665bc928ac33bdac7b1559e
leeMK09 <dlaudrb09@naver.com>
2025-02-21 14:05:40 +0900

    fix: 변경된 부분 컴파일 이슈 수정

--- kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/domain/payment/WinningStatisticsTest.kt
+++ kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/domain/payment/WinningStatisticsTest.kt
@@ -11,26 +11,29 @@ class WinningStatisticsTest {
 
         val threeMatchedPrize = LottoStatisticsSheet.THREE_MATCHED.amount * threeMatchedCount
         val fourMatchedPrize = LottoStatisticsSheet.FOUR_MATCHED.amount * fourMatchedCount
-        val expectedTotalPrize = threeMatchedPrize + fourMatchedPrize
+        val expectedTotalPrize = Payment(threeMatchedPrize + fourMatchedPrize)
 
-        val statistics = mapOf(
-            LottoStatisticsSheet.THREE_MATCHED to threeMatchedCount,
-            LottoStatisticsSheet.FOUR_MATCHED to fourMatchedCount
-        )
+        val statistics =
+            mapOf(
+                LottoStatisticsSheet.THREE_MATCHED to threeMatchedCount,
+                LottoStatisticsSheet.FOUR_MATCHED to fourMatchedCount,
+            )
         val winningStatistics = WinningStatistics(statistics)
 
-        val totalPrizeMoney = winningStatistics.totalPrizeMoney()
+        val totalPrizeMoney = winningStatistics.totalPrizePayment()
+        val result = totalPrizeMoney.getPaidMoney()
 
-        assertEquals(expectedTotalPrize, totalPrizeMoney)
+        val expected = expectedTotalPrize.getPaidMoney()
+        assertEquals(expected, result)
     }
-
 
     @Test
     fun `당첨 통계를 올바르게 반환해야 한다`() {
-        val statistics = mapOf(
-            LottoStatisticsSheet.THREE_MATCHED to 1,
-            LottoStatisticsSheet.FIVE_MATCHED to 1
-        )
+        val statistics =
+            mapOf(
+                LottoStatisticsSheet.THREE_MATCHED to 1,
+                LottoStatisticsSheet.FIVE_MATCHED to 1,
+            )
         val winningStatistics = WinningStatistics(statistics)
 
         val result = winningStatistics.getStatistics()
