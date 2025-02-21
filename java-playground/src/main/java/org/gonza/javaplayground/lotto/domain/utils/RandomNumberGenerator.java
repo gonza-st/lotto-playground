048a7b3090fc261bbac0a4ddbded1741d06fe172
han-0624 <han@wemeetmobility.com>
2025-02-21 15:08:03 +0900

    review: 인풋에 대한 벨리데이션 추가

--- java-playground/src/main/java/org/gonza/javaplayground/lotto/domain/utils/RandomNumberGenerator.java
+++ java-playground/src/main/java/org/gonza/javaplayground/lotto/domain/utils/RandomNumberGenerator.java
@@ -2,23 +2,38 @@ package org.gonza.javaplayground.lotto.domain.utils;
 
 import org.gonza.javaplayground.lotto.domain.lotto.NumberGenerator;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class RandomNumberGenerator implements NumberGenerator {
     @Override
     public List<Integer> generate(Integer size, Integer min, Integer max) {
-        if (min > max) {
-            throw new IllegalArgumentException("min must be less than max");
-        }
+        validateInteger(size, min, max);
 
         List<Integer> shuffledNumbers = generateShuffledNumbers(min, max);
         List<Integer> limited = limitedNumbers(shuffledNumbers, size);
 
         return limited;
     }
+
+    private void validateInteger(Integer size, Integer min, Integer max) {
+        if (Objects.isNull(size) || Objects.isNull(min) || Objects.isNull(max)) {
+            throw new IllegalArgumentException("min and/or max must be non-null");
+        }
+
+        if (size <= 0 || min <= 0 || max <= 0) {
+            throw new IllegalArgumentException("min and max must be greater than zero");
+        }
+
+        if (min > max) {
+            throw new IllegalArgumentException("min must be less than max");
+        }
+    }
+
 
     private List<Integer> generateShuffledNumbers(Integer min, Integer max) {
         List<Integer> numbers = IntStream.range(min, max + 1)
