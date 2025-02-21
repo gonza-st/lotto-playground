64f9985bf0d1f7f89af9e70e841a954dc7ab463c
currenjin <hyun0524e@naver.com>
2025-02-21 13:55:52 +0900

    Improve: Validate lotto numbers

--- java-playground/src/main/java/org/gonza/javaplayground/lotto/domain/LottoNumber.java
+++ java-playground/src/main/java/org/gonza/javaplayground/lotto/domain/LottoNumber.java
@@ -5,7 +5,26 @@ import java.util.List;
 
 public class LottoNumber {
     public static LottoNumber of(List<Integer> numbers) {
+        validateNumbers(numbers);
+
         return new LottoNumber(numbers);
+    }
+
+    private static void validateNumbers(List<Integer> numbers) {
+        validateSize(numbers);
+        numbers.forEach(LottoNumber::validateRange);
+    }
+
+    private static void validateSize(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("Number must be 6 numbers");
+        }
+    }
+
+    private static void validateRange(Integer number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("Number must be between 1 and 45");
+        }
     }
 
     private final List<Integer> numbers;
