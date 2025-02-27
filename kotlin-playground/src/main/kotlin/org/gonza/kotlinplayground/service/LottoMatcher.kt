91d28bec17fe2cd0521a5a11252b502e763d47d5
leeMK09 <dlaudrb09@naver.com>
2025-02-27 13:45:53 +0900

    chore: 불필요한 메서드 제거 (당첨된 통계정보 가져오기)

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/service/LottoMatcher.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/service/LottoMatcher.kt
@@ -1,17 +1,11 @@
 package org.gonza.kotlinplayground.service
 
 import org.gonza.kotlinplayground.domain.lotto.LottoTicket
-import org.gonza.kotlinplayground.domain.payment.LottoStatisticsSheet
 import org.gonza.kotlinplayground.domain.payment.WinningStatistics
 
 interface LottoMatcher {
-    fun getMatchedStatisticSheetList(
-        result: LottoTicket,
-        ticket: LottoTicket,
-    ): LottoStatisticsSheet?
-
     fun getWinningStatistics(
         result: LottoTicket,
-        purchasedTicketList: List<LottoTicket>
+        purchasedTicketList: List<LottoTicket>,
     ): WinningStatistics
 }
