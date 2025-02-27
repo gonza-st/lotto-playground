211c6e6241b481b2a21db54903b1ee53bc0c1775
leeMK09 <dlaudrb09@naver.com>
2025-02-27 13:52:24 +0900

    refactor: 당첨티켓을 통해 당첨 통계정보를 가져오는 부분을 메서드로 분리

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/service/GeneralLottoMatcher.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/service/GeneralLottoMatcher.kt
@@ -8,11 +8,7 @@ class GeneralLottoMatcher(
     private val purchasedTicketList: List<LottoTicket>,
 ) : LottoMatcher {
     override fun getWinningStatistics(result: LottoTicket): WinningStatistics {
-        val matchedStatistics =
-            purchasedTicketList.mapNotNull { ticket ->
-                val matchedNumberList = result.getMatchedNumber(ticket)
-                LottoStatisticsSheet.findByMatchedNumber(matchedNumberList.size)
-            }
+        val matchedStatistics = getMatchedLottoStatisticsSheet(result)
 
         val winningCountList = getWinningCountList(matchedStatistics)
 
@@ -20,6 +16,12 @@ class GeneralLottoMatcher(
             winningCountList = winningCountList,
         )
     }
+
+    private fun getMatchedLottoStatisticsSheet(result: LottoTicket) =
+        purchasedTicketList.mapNotNull { ticket ->
+            val matchedNumberList = result.getMatchedNumber(ticket)
+            LottoStatisticsSheet.findByMatchedNumber(matchedNumberList.size)
+        }
 
     private fun getWinningCountList(matchedStatistics: List<LottoStatisticsSheet>): List<WinningCount> {
         val groupedWonStatistics = matchedStatistics.groupingBy { it }
