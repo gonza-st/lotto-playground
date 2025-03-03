e51dcbdc0f15b1d0b160279ace4a1c2f3341f3bc
leemk09 <dlaudrb09@naver.com>
2025-03-03 20:26:04 +0900

    feat: 보너스 번호에 대한 로또 매처 추가

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/service/BonusNumberMatcher.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/service/BonusNumberMatcher.kt
@@ -1,14 +1,52 @@
 package org.gonza.kotlinplayground.service
 
-import org.gonza.kotlinplayground.domain.lotto.BonusNumber
+import org.gonza.kotlinplayground.domain.lotto.LottoNumber
 import org.gonza.kotlinplayground.domain.lotto.LottoTicket
+import org.gonza.kotlinplayground.domain.payment.LottoStatisticsSheet
 import org.gonza.kotlinplayground.domain.payment.WinningStatistics
 
 class BonusNumberMatcher(
     private val result: LottoTicket,
-    private val bonusNumber: BonusNumber
+    private val bonusNumber: LottoNumber,
 ) : LottoMatcher {
+    companion object {
+        private val BONUS_STATISTICS_SHEET = LottoStatisticsSheet.BONUS_MATCHED
+    }
+
     override fun getWinningStatistics(purchasedTicketList: List<LottoTicket>): WinningStatistics {
-        TODO("Not yet implemented")
+        val matchedStatistics =
+            purchasedTicketList.mapNotNull { ticket ->
+                findBonusStatistics(ticket)
+            }
+
+        return WinningStatistics(
+            winningCountList = createWinningCountList(matchedStatistics),
+        )
+    }
+
+    private fun findBonusStatistics(purchasedTicket: LottoTicket): LottoStatisticsSheet? {
+        val matchedLottoNumberList = result.getMatchedNumber(purchasedTicket)
+
+        if (!isBonus(matchedLottoNumberList.size)) return null
+
+        if (!purchasedTicket.isBonus(bonusNumber)) return null
+
+        return BONUS_STATISTICS_SHEET
+    }
+
+    private fun createWinningCountList(bonusStatisticsList: List<LottoStatisticsSheet>): List<WinningCount> =
+        listOf(
+            WinningCount(
+                statisticsSheet = BONUS_STATISTICS_SHEET,
+                count = bonusStatisticsList.size,
+            ),
+        )
+
+    private fun isBonus(matchedCount: Int): Boolean = BONUS_STATISTICS_SHEET.isMatched(matchedCount)
+
+    private fun LottoTicket.isBonus(bonusNumber: LottoNumber): Boolean {
+        val matchedNumber = this.getMatchedNumber(bonusNumber)
+
+        return matchedNumber != null
     }
 }
--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/service/GeneralLottoMatcher.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/service/GeneralLottoMatcher.kt
@@ -5,25 +5,27 @@ import org.gonza.kotlinplayground.domain.payment.LottoStatisticsSheet
 import org.gonza.kotlinplayground.domain.payment.WinningStatistics
 
 class GeneralLottoMatcher(
-    private val result: LottoTicket
+    private val result: LottoTicket,
 ) : LottoMatcher {
     override fun getWinningStatistics(purchasedTicketList: List<LottoTicket>): WinningStatistics {
-        val matchedStatistics = purchasedTicketList.mapNotNull { ticket ->
-            val matchedLottoNumberList = result.getMatchedNumber(ticket)
-            LottoStatisticsSheet.findByMatchedCount(matchedLottoNumberList.size)
-        }
+        val matchedStatistics =
+            purchasedTicketList.mapNotNull { ticket ->
+                val matchedLottoNumberList = result.getMatchedNumber(ticket)
+                LottoStatisticsSheet.findByMatchedCount(matchedLottoNumberList.size)
+            }
 
         val winningCountList = getWinningCountList(matchedStatistics)
 
         return WinningStatistics(
-            winningCountList = winningCountList
+            winningCountList = winningCountList,
         )
     }
 
     private fun getWinningCountList(matchedStatistics: List<LottoStatisticsSheet>): List<WinningCount> {
         val groupedWonStatistics = matchedStatistics.groupingBy { it }
 
-        return groupedWonStatistics.eachCount()
+        return groupedWonStatistics
+            .eachCount()
             .map { (sheet, count) ->
                 WinningCount(sheet, count)
             }
