1a6083fe41f463d71a221cd654544d198a7d1f7a
leemk09 <dlaudrb09@naver.com>
2025-03-03 18:56:54 +0900

    chore: 불필요한 기능 삭제

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/service/GeneralLottoMatcher.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/service/GeneralLottoMatcher.kt
@@ -5,17 +5,6 @@ import org.gonza.kotlinplayground.domain.payment.LottoStatisticsSheet
 import org.gonza.kotlinplayground.domain.payment.WinningStatistics
 
 class GeneralLottoMatcher : LottoMatcher {
-    override fun getMatchedStatisticSheetList(
-        result: LottoTicket,
-        ticket: LottoTicket,
-    ): LottoStatisticsSheet? {
-        val matchedNumberList = result.getMatchedNumber(ticket)
-        val allStatisticSheetList = LottoStatisticsSheet.sortedByDescending()
-        return allStatisticSheetList.find {
-            it.isMatched(matchedNumberList.size)
-        }
-    }
-
     override fun getWinningStatistics(result: LottoTicket, purchasedTicketList: List<LottoTicket>): WinningStatistics {
         val matchedStatistics = purchasedTicketList.mapNotNull { ticket ->
             val matchedLottoNumberList = result.getMatchedNumber(ticket)
--- kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/service/GeneralLottoMatcherTest.kt
+++ kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/service/GeneralLottoMatcherTest.kt
@@ -6,42 +6,6 @@ import org.junit.jupiter.api.Test
 
 class GeneralLottoMatcherTest {
     @Test
-    fun `당첨 로또 와 뽑은 로또를 입력하면 통계 시트내에서 맞춘 통계 시트 목록을 알 수 있다`() {
-        val result =
-            LottoMatcherFixture.createLottoTicket(
-                listOf(1, 2, 3, 4, 5, 6),
-            )
-        val ticketList =
-            LottoMatcherFixture.createLottoTicketList(
-                listOf(
-                    listOf(1, 2, 3, 7, 8, 9),
-                    listOf(1, 2, 3, 4, 8, 9),
-                    listOf(1, 2, 3, 4, 5, 9),
-                    listOf(1, 2, 3, 4, 5, 6),
-                ),
-            )
-        val lottoMatcher = GeneralLottoMatcher()
-
-        val statisticSheetList =
-            ticketList.map { ticket ->
-                lottoMatcher.getMatchedStatisticSheetList(
-                    result = result,
-                    ticket = ticket,
-                )
-            }
-
-        val expected =
-            listOf(
-                LottoStatisticsSheet.THREE_MATCHED,
-                LottoStatisticsSheet.FOUR_MATCHED,
-                LottoStatisticsSheet.FIVE_MATCHED,
-                LottoStatisticsSheet.ALL_MATCHED,
-            )
-        assertEquals(expected, statisticSheetList)
-        assertEquals(statisticSheetList.size, expected.size)
-    }
-
-    @Test
     fun `로또 당첨 결과를 올바르게 계산해야 한다`() {
         val winningTicket = LottoMatcherFixture.createLottoTicket(
             listOf(1, 2, 3, 4, 5, 6)
