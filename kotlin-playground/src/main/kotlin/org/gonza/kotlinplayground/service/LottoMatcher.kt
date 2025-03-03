ac0113a49c1d300809693de970fb5fdb487413cf
leemk09 <dlaudrb09@naver.com>
2025-03-03 18:56:54 +0900

    chore: 불필요한 기능 삭제

--- kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/service/LottoMatcher.kt
+++ kotlin-playground/src/main/kotlin/org/gonza/kotlinplayground/service/LottoMatcher.kt
@@ -5,11 +5,6 @@ import org.gonza.kotlinplayground.domain.payment.LottoStatisticsSheet
 import org.gonza.kotlinplayground.domain.payment.WinningStatistics
 
 interface LottoMatcher {
-    fun getMatchedStatisticSheetList(
-        result: LottoTicket,
-        ticket: LottoTicket,
-    ): LottoStatisticsSheet?
-
     fun getWinningStatistics(
         result: LottoTicket,
         purchasedTicketList: List<LottoTicket>
