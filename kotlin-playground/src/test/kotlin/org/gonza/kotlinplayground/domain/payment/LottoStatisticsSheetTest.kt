3220602f0af5244f7a0a5604fc05b0158b5d308c
leemk09 <dlaudrb09@naver.com>
2025-03-03 18:53:18 +0900

    refactor: 당첨 로또 숫자와 일치하는 개수를 통해 해당 통계정보를 가져올 수 있다

--- kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/domain/payment/LottoStatisticsSheetTest.kt
+++ kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/domain/payment/LottoStatisticsSheetTest.kt
@@ -18,4 +18,22 @@ class LottoStatisticsSheetTest {
 
         assertEquals(expected, result)
     }
+
+    @Test
+    fun `통계정보와 일치하는 개수를 입력하면 알맞은 통계정보를 조회할 수 있다`() {
+        val threeMatchedCount = 3
+        val fourMatchedCount = 4
+        val fiveMatchedCount = 5
+        val allMatchedCount = 6
+
+        val threeStatistics = LottoStatisticsSheet.findByMatchedCount(threeMatchedCount)!!
+        val fourStatistics = LottoStatisticsSheet.findByMatchedCount(fourMatchedCount)!!
+        val fiveStatistics = LottoStatisticsSheet.findByMatchedCount(fiveMatchedCount)!!
+        val allStatistics = LottoStatisticsSheet.findByMatchedCount(allMatchedCount)!!
+
+        assertEquals(threeStatistics.matchedCount, threeMatchedCount)
+        assertEquals(fourStatistics.matchedCount, fourMatchedCount)
+        assertEquals(fiveStatistics.matchedCount, fiveMatchedCount)
+        assertEquals(allStatistics.matchedCount, allMatchedCount)
+    }
 }
