0942fa2c33daf9bd723339ff4a01e7d457b91131
leemk09 <dlaudrb09@naver.com>
2025-03-03 19:27:20 +0900

    feat: 보너스 통계 정보 추가

--- kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/domain/payment/LottoStatisticsSheetTest.kt
+++ kotlin-playground/src/test/kotlin/org/gonza/kotlinplayground/domain/payment/LottoStatisticsSheetTest.kt
@@ -5,21 +5,6 @@ import org.junit.jupiter.api.Test
 
 class LottoStatisticsSheetTest {
     @Test
-    fun `통계 정보를 일치하는 숫자 기준으로 오름차순 정렬로 가져올 수 있다`() {
-        val expected =
-            listOf(
-                LottoStatisticsSheet.ALL_MATCHED,
-                LottoStatisticsSheet.FIVE_MATCHED,
-                LottoStatisticsSheet.FOUR_MATCHED,
-                LottoStatisticsSheet.THREE_MATCHED,
-            )
-
-        val result = LottoStatisticsSheet.sortedByDescending()
-
-        assertEquals(expected, result)
-    }
-
-    @Test
     fun `통계정보와 일치하는 개수를 입력하면 알맞은 통계정보를 조회할 수 있다`() {
         val threeMatchedCount = 3
         val fourMatchedCount = 4
